ad5abedcb75401b4e5ef158c0287cf4b8ffb3973
Beka <bektemir.abdymomunov@mail.ru>
2021-01-26 14:52:55 -0500

    added awesome codes

--- src/test/java/FeatureXXX_test.java
+++ src/test/java/FeatureXXX_test.java
@@ -2,5 +2,6 @@ public class FeatureXXX_test {
     public static void main(String[] args) {
         System.out.println("WOLAAAAA");
         System.out.println("git->merge->no-off-m");
+        System.out.println("Currently on feature xxx class");
     }
 }
