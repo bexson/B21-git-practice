f11427235f8f62e5509e9d265a22293f26a4bc27
Beka <bektemir.abdymomunov@mail.ru>
2021-01-26 15:39:22 -0500

    master branch watching on the living room for 10 min

--- src/test/java/FeatureBClass.java
+++ src/test/java/FeatureBClass.java
@@ -3,4 +3,5 @@ public class FeatureBClass {
     public static void main(String[] args) {
         System.out.println("initial commit");
      }
+     // oh i like this movie , who played it ? let me continue
 }
