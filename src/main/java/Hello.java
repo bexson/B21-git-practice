87a8537f339750f6fe716d8d0fe1fca9ad37dd8e
Beka <bektemir.abdymomunov@mail.ru>
2021-01-26 15:22:59 -0500

    something added

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello, WORLD, B21");
         System.out.println("HiHello");
+        //watching for movie
     }
 }
