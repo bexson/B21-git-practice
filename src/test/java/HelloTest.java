4868a537d5079ed2c63d5478ec5b0ab0b8281d9f
Beka <bektemir.abdymomunov@mail.ru>
2021-01-26 15:23:59 -0500

    spouse changed the movie

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,8 @@ public class HelloTest {
 
     //testing my awesome hello
     //EMREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    public static void main(String[] args) {
+        System.out.println("hellooo");
+    }
 
 }
