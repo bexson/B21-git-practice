5cc386199de857f9e455e1825dfe4814422cf75d
Beka <bektemir.abdymomunov@mail.ru>
2021-01-26 19:06:12 -0500

    added new line in main method for HelloTest.java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,10 +3,7 @@ public class HelloTest {
     //testing my awesome hello
     //EMREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
     public static void main(String[] args) {
-        System.out.println("hellooo");
-        //movie on the phone 10 min
-        //movie on the phone 20 min
-        //another final 10 min before going back to living room
+        //I HAVE MY MAIN METHOD ON MY MASTER BRANCH
     }
 
 }
